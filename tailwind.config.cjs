70905daac99952715113c52e10de26afcf66e662
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2023-01-22 22:17:52 -0500

    update tailwaindconfig

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#15001f",
+        secondary: "#9500ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimPurple: "rgba(140, 20, 252, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
